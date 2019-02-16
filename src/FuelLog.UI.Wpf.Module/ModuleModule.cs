4f6c5dd57bb13aee215d0f39194e23186ab2b2f2
Peter Centellini <peter.c@trivec.se>
2019-02-16 19:14:01 +0100

    Added Prism Shell & Module projects

---
+++ src/FuelLog.UI.Wpf.Module/ModuleModule.cs
@@ -0,0 +1,20 @@
+﻿using FuelLog.UI.Wpf.Module.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace FuelLog.UI.Wpf.Module
+{
+    public class ModuleModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+ 
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
+    }
+}
