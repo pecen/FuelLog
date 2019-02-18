63341bbe9bcd78540a5221c3e0b199907444ae0e
Peter Centellini <peter.c@trivec.se>
2019-02-18 08:57:49 +0100

    Added nuget.config

--- src/FuelLog.UI.Wpf.Module/FuelLogModule.cs
+++ src/FuelLog.UI.Wpf.Module/FuelLogModule.cs
@@ -2,19 +2,27 @@
 using Prism.Ioc;
 using Prism.Modularity;
 using Prism.Regions;
+using Unity;
 
-namespace FuelLog.UI.Wpf.Module
-{
-    public class FuelLogModule : IModule
-    {
-        public void OnInitialized(IContainerProvider containerProvider)
-        {
- 
-        }
+namespace FuelLog.UI.Wpf.Module {
+  public class FuelLogModule : IModule {
+    private IRegionManager _regionManager;
+    private IUnityContainer _container;
 
-        public void RegisterTypes(IContainerRegistry containerRegistry)
-        {
-            
-        }
+    public FuelLogModule(IRegionManager regionManager, IUnityContainer container) {
+      _regionManager = regionManager;
+      _container = container;
     }
+
+    public void Initialize() {
+      _regionManager.RegisterViewWithRegion("TabRegion", typeof(ViewA));
+    }
+
+    public void OnInitialized(IContainerProvider containerProvider) {
+    }
+
+    public void RegisterTypes(IContainerRegistry containerRegistry) {
+      containerRegistry.RegisterForNavigation<ViewA>("ViewA");
+    }
+  }
 }
