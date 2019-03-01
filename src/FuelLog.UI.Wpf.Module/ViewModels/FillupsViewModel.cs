d66169e035dde929bffaee16072fb25e70ada8ef
Peter Centellini <peter.c@trivec.se>
2019-03-01 12:03:44 +0100

    Added the Fillups list view

---
+++ src/FuelLog.UI.Wpf.Module/ViewModels/FillupsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using FuelLog.UI.Wpf.Module.Enums;
+using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FuelLog.UI.Wpf.Module.ViewModels {
+  public class FillupsViewModel : ViewModelBase {
+    public FillupsViewModel() {
+      Title = TabHeaders.Fillups.ToString();
+    }
+  }
+}
---
+++ src/FuelLog.UI.Wpf.Module/Views/Fillups.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace FuelLog.UI.Wpf.Module.Views
+{
+    /// <summary>
+    /// Interaction logic for Fillups
+    /// </summary>
+    public partial class Fillups : UserControl
+    {
+        public Fillups()
+        {
+            InitializeComponent();
+        }
+    }
+}
