a3fdc6d4ab1f775c9ed38d7a4b343b8b6fc50a27
Peter Centellini <peter.c@trivec.se>
2019-03-01 12:34:40 +0100

    Added Fillups to MockDb

---
+++ src/FuelLog.DalMock/MockDb/FillupData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuelLog.DalMock.MockDb {
+  public class FillupData {
+    public int Id { get; set; }
+    public int CarId { get; set; }
+    public DateTime FillUpDate { get; set; }
+    public int Odometer { get; set; }
+    public double Amount { get; set; }
+    public double VolumePrice { get; set; }
+    public bool PartialFillUp { get; set; }
+    public string Note { get; set; }
+  }
+}
