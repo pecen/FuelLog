04716595193d46fd184993dfd78d0b7479670c51
Peter Centellini <peter.centellini@redesajn.com>
2019-03-02 16:12:48 +0100

    Added AddCar view

---
+++ src/FuelLog.UI.Wpf.Module/ViewModels/AddCarViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FuelLog.UI.Wpf.Module.ViewModels {
+  public class AddCarViewModel : BindableBase {
+    public AddCarViewModel() {
+
+    }
+  }
+}
