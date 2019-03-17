809102c95fbdbfd6e4c014efe22ed8ed6bd4f738
Peter Centellini <peter.centellini@redesajn.com>
2019-03-17 23:39:25 +0100

    Completed the visual items in AddCar. Added properties to CarEdit

--- src/FuelLog.Library/CarEdit.cs
+++ src/FuelLog.Library/CarEdit.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Csla;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,97 @@ using System.Threading.Tasks;
 
 namespace FuelLog.Library
 {
-    public class CarEdit
+  [Serializable]
+  public class CarEdit : BusinessBase<CarEdit>
     {
+    #region Properties
+
+    public static readonly PropertyInfo<int> IdProperty = RegisterProperty<int>(c => c.Id);
+    public int Id {
+      get { return GetProperty(IdProperty); }
+      set { SetProperty(IdProperty, value); }
     }
+
+    public static readonly PropertyInfo<string> MakeProperty = RegisterProperty<string>(c => c.Make);
+    public string Make {
+      get { return GetProperty(MakeProperty); }
+      set { SetProperty(MakeProperty, value); }
+    }
+
+    public static readonly PropertyInfo<string> ModelProperty = RegisterProperty<string>(c => c.Model);
+    public string Model {
+      get { return GetProperty(ModelProperty); }
+      set { SetProperty(ModelProperty, value); }
+    }
+
+    public string FullName {
+      get { return Make + " " + Model; }
+    }
+
+    public static readonly PropertyInfo<string> LicensePlateProperty = RegisterProperty<string>(c => c.LicensePlate);
+    public string LicensePlate {
+      get { return GetProperty(LicensePlateProperty); }
+      set { SetProperty(LicensePlateProperty, value); }
+    }
+
+    public static readonly PropertyInfo<string> NoteProperty = RegisterProperty<string>(c => c.Note);
+    public string Note {
+      get { return GetProperty(NoteProperty); }
+      set { SetProperty(NoteProperty, value); }
+    }
+
+    public static readonly PropertyInfo<string> DistanceUnitProperty = RegisterProperty<string>(c => c.DistanceUnit);
+    public string DistanceUnit {
+      get { return GetProperty(DistanceUnitProperty); }
+      set { SetProperty(DistanceUnitProperty, value); }
+    }
+
+    public static readonly PropertyInfo<string> VolumeUnitProperty = RegisterProperty<string>(c => c.VolumeUnit);
+    public string VolumeUnit {
+      get { return GetProperty(VolumeUnitProperty); }
+      set { SetProperty(VolumeUnitProperty, value); }
+    }
+
+    public static readonly PropertyInfo<string> ConsumptionUnitProperty = RegisterProperty<string>(c => c.ConsumptionUnit);
+    public string ConsumptionUnit {
+      get { return GetProperty(ConsumptionUnitProperty); }
+      set { SetProperty(ConsumptionUnitProperty, value); }
+    }
+
+    public string ChosenUnits {
+      get { return DistanceUnit + ", " + VolumeUnit + ", " + ConsumptionUnit; }
+    }
+
+    public static readonly PropertyInfo<string> TotalFillupsProperty = RegisterProperty<string>(c => c.TotalFillups);
+    public string TotalFillups {
+      get { return GetProperty(TotalFillupsProperty); }
+      set { SetProperty(TotalFillupsProperty, value); }
+    }
+
+    public static readonly PropertyInfo<string> TotalDistanceProperty = RegisterProperty<string>(c => c.TotalDistance);
+    public string TotalDistance {
+      get { return GetProperty(TotalDistanceProperty); }
+      set { SetProperty(TotalDistanceProperty, value); }
+    }
+
+    public static readonly PropertyInfo<string> AverageConsumptionProperty = RegisterProperty<string>(c => c.AverageConsumption);
+    public string AverageConsumption {
+      get { return GetProperty(AverageConsumptionProperty); }
+      set { SetProperty(AverageConsumptionProperty, value); }
+    }
+
+    public static readonly PropertyInfo<DateTimeOffset> DateAddedProperty = RegisterProperty<DateTimeOffset>(c => c.DateAdded);
+    public DateTimeOffset DateAdded {
+      get { return GetProperty(DateAddedProperty); }
+      set { SetProperty(DateAddedProperty, value); }
+    }
+
+    public static readonly PropertyInfo<DateTimeOffset> LastModifiedProperty = RegisterProperty<DateTimeOffset>(c => c.LastModified);
+    public DateTimeOffset LastModified {
+      get { return GetProperty(LastModifiedProperty); }
+      set { SetProperty(LastModifiedProperty, value); }
+    }
+
+    #endregion
+  }
 }
--- src/FuelLog.UI.Wpf.Module/ViewModels/AddCarViewModel.cs
+++ src/FuelLog.UI.Wpf.Module/ViewModels/AddCarViewModel.cs
@@ -1,12 +1,18 @@
-﻿using FuelLog.UI.Wpf.Module.Enums;
+﻿using FuelLog.Library;
+using FuelLog.UI.Wpf.Module.Enums;
 using Prism.Commands;
 using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 
 namespace FuelLog.UI.Wpf.Module.ViewModels {
   public class AddCarViewModel : ViewModelBase {
+    public ObservableCollection<CarInfo> DistanceUnit { get; set; }
+    public ObservableCollection<CarInfo> VolumeUnit { get; set; }
+    public ObservableCollection<CarInfo> ConsumptionUnit { get; set; }
+
     public AddCarViewModel() {
       Title = TabHeaders.AddCar.ToString();
     }
