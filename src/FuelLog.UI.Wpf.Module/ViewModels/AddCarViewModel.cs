ea2a298b96cf924a1e6ae2495e4d7dfaedaaa89a
Peter Centellini <peter.c@trivec.se>
2019-03-18 12:58:49 +0100

    Added Save and Cancel buttons to AddCar. Implemented CanExecute for Save.

--- src/FuelLog.UI.Wpf.Module/ViewModels/AddCarViewModel.cs
+++ src/FuelLog.UI.Wpf.Module/ViewModels/AddCarViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using FuelLog.Library;
 using FuelLog.UI.Wpf.Module.Enums;
 using Prism.Commands;
+using Prism.Events;
 using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
@@ -9,12 +10,43 @@ using System.Linq;
 
 namespace FuelLog.UI.Wpf.Module.ViewModels {
   public class AddCarViewModel : ViewModelBase {
+    private readonly IEventAggregator _eventAggregator;
+
     public ObservableCollection<CarInfo> DistanceUnit { get; set; }
     public ObservableCollection<CarInfo> VolumeUnit { get; set; }
     public ObservableCollection<CarInfo> ConsumptionUnit { get; set; }
 
-    public AddCarViewModel() {
+    private string _make;
+    public string Make {
+      get { return _make; }
+      set { SetProperty(ref _make, value); }
+    }
+
+    private string _model;
+    public string Model {
+      get { return _model; }
+      set { SetProperty(ref _model, value); }
+    }
+
+    public DelegateCommand SaveCommand { get; set; }
+
+    public AddCarViewModel(IEventAggregator eventAggregator) {
       Title = TabHeaders.AddCar.ToString();
+
+      _eventAggregator = eventAggregator;
+
+      SaveCommand = new DelegateCommand(Execute, CanExecute)
+        .ObservesProperty(() => Make)
+        .ObservesProperty(() => Model);
+    }
+
+    private bool CanExecute() {
+      return !string.IsNullOrWhiteSpace(Make)
+        && !string.IsNullOrWhiteSpace(Model);
+    }
+
+    private void Execute() {
+      throw new NotImplementedException();
     }
   }
 }
