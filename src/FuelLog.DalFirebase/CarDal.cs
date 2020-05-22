a82e4283a0a1662a69f52684e6499c3272a7ab5f
Peter Centellini <peter.c@trivec.se>
2020-05-22 14:46:41 +0200

    Started with DalFirebase

---
+++ src/FuelLog.DalFirebase/CarDal.cs
@@ -0,0 +1,43 @@
+﻿using FuelLog.Dal;
+using FuelLog.Dal.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuelLog.DalFirebase {
+  public class CarDal : ICarDal {
+    public void Delete(int id) {
+      throw new NotImplementedException();
+    }
+
+    public IList<int> DeleteRange(int[] ids) {
+      throw new NotImplementedException();
+    }
+
+    public bool Exists(string licensePlate) {
+      throw new NotImplementedException();
+    }
+
+    public List<CarDto> Fetch() {
+      throw new NotImplementedException();
+    }
+
+    public CarDto Fetch(int id) {
+      throw new NotImplementedException();
+    }
+
+    public CarDto Fetch(string licenseNumber) {
+      throw new NotImplementedException();
+    }
+
+    public void Insert(CarDto data) {
+      throw new NotImplementedException();
+    }
+
+    public void Update(CarDto data) {
+      throw new NotImplementedException();
+    }
+  }
+}
